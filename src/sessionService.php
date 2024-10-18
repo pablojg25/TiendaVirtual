c229690eb7f026e9e368743ac3479091db38482d
pablojg25 <pablojorgegorrn@gmail.com>
2024-10-18 16:42:58 +0100

    sessionService toString

--- src/sessionService.php
+++ src/sessionService.php
@@ -4,12 +4,16 @@ namespace Dsw\TiendaVirtual;
 class SessionService extends Service implements Session {
     private int $sessionNumber;
 
-    public function constructor($name, $basePrice, $sessionNumber) {
+    public function __construct($name, $basePrice, $sessionNumber) {
         parent::__construct($name, $basePrice);
         $this->sessionNumber = $sessionNumber;
     }
 
     public function useSession() {
         $this->sessionNumber--;
+    }
+
+    public function __toString() {
+        return sprintf("%s, quedan %d sesiones",parent::__toString(),$this->sessionNumber);
     }
 }
